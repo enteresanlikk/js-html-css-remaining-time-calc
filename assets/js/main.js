937516e75ca1af762bb9293fe15ac8819e9ea94a
Bilal <zeki1198@gmail.com>
2018-10-03 22:51:52 +0300

    Project updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,53 +3,38 @@ document.querySelector('#form').addEventListener('submit',(e)=>{
     document.querySelector('input[type="reset"]').classList.remove('hide');
 
     const form=e.currentTarget;
+    const multipler = form.elements['multipler'];
+    const fileSize = form.elements['fileSize'];
+    const sizeType = form.elements['sizeType'];
+    const speedSize = form.elements['speed'];
+    const speedType = form.elements['speedType'];
 
-    let multipler = form.elements['multipler'].value;
-    let fileSize = form.elements['fileSize'].value;
-    let sizeType = form.elements['sizeType'].value;
-    let speedSize = form.elements['speed'].value;
-    let speedType = form.elements['speedType'].value;
-
-    let size = CalcSize(fileSize, sizeType, multipler);
-
-    let speed = CalcSpeed(speedSize, speedType, multipler);
-
-    let time = ConvertToTime(Math.round(size/speed));
+    const size = CalcSize(fileSize.value, sizeType.value, multipler.value);
+    const speed = CalcSpeed(speedSize.value, speedType.value, multipler.value);
+    const time = ConvertToTime(Math.round(size/speed));
 
     const resultContent = document.querySelector('.result');
     resultContent.classList.remove('hide');
 
-    document.querySelector('#resSpeed').innerHTML = `${speedSize} ${speedType}`;
-    document.querySelector('#resSize').innerHTML = `${fileSize} ${sizeType.toUpperCase()}`;
+    document.querySelector('#resSpeed').innerHTML = `${speedSize.value} ${speedType[speedType.value].text}`;
+    document.querySelector('#resSize').innerHTML = `${fileSize.value} ${sizeType[sizeType.value].text}`;
     document.querySelector('#resTime').innerHTML = `${time}`;
 
     e.preventDefault();
 });
 
 CalcSize=(size, type, multipler)=>{
-    switch(type){
-        case 'bit': return size;
-        case 'byte': return ConvertSize(size, multipler, 0);
-        case 'kb': return ConvertSize(size, multipler);
-        case 'mb': return ConvertSize(size, multipler, 2);
-        case 'gb': return ConvertSize(size, multipler, 3);
-        case 'tb': return ConvertSize(size, multipler, 4);
-        case 'pt': return ConvertSize(size, multipler, 5);
-        case 'eb': return ConvertSize(size, multipler, 6);
-    }
+    if(type <= -1)
+        return size;
+    else
+        return ConvertSize(size, multipler, parseInt(type));
 }
 
 CalcSpeed=(size, type, multipler)=>{
-    switch(type){
-        case 'bps': return size;
-        case 'kbps': return ConvertSpeedSize(size, multipler, 1);
-        case 'mbps': return ConvertSpeedSize(size, multipler, 2);
-        case 'gbps': return ConvertSpeedSize(size, multipler, 3);
-        case 'bs': return ConvertSpeedSize(size, multipler, 4);
-        case 'kbs': return ConvertSpeedSize(size, multipler, 5);
-        case 'mbs': return ConvertSpeedSize(size, multipler, 6);
-        case 'gbs': return ConvertSpeedSize(size, multipler, 7);
-    }
+    if(type <= -1)
+        return size;
+    else
+        return ConvertSpeedSize(size, multipler, parseInt(type));
 }
 
 ConvertSize = (size, factor, repeat = 1) => {
@@ -67,14 +52,10 @@ ConvertSpeedSize = (size, factor, repeat = 1) => {
 }
 
 ConvertToTime=(time)=>{
-    const sec_num = parseInt(time, 10); // don't forget the second param
+    const sec_num = parseInt(time, 10);
     let hours   = Math.floor(sec_num / 3600);
     let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     let seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-    // if (hours   < 10) {hours   = "0"+hours;}
-    // if (minutes < 10) {minutes = "0"+minutes;}
-    // if (seconds < 10) {seconds = "0"+seconds;}
     return (hours=="00" ? '' : hours+' hours, ' ) + (hours=='00' && minutes =='00' ? '' : minutes + ' minutes and ') + seconds+ ' seconds';
 }
 
